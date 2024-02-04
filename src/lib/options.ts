b0737f5bf29ae0c81eef78bdba7a0b60bc3d3bfc
Sergio Afanador <sergioafanador102@gmail.com>
2024-02-04 12:01:28 -0500

    debugging 1.6 Auth options separeted

--- src/lib/options.ts
+++ src/lib/options.ts
@@ -4,9 +4,9 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import type { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
-import NextAuth from "next-auth";
 
-const authOptions: NextAuthOptions = {
+
+export const authOptions: NextAuthOptions = {
     adapter:PrismaAdapter(prisma) as Adapter,
     providers:[
         GoogleProvider({
@@ -15,7 +15,3 @@ const authOptions: NextAuthOptions = {
         })
     ]
 }
-
-const handler = NextAuth(authOptions);
-
-export{handler as GET, handler as POST}
