44f859f3ea11ec0f95707f3e43ff7d53064fff1b
Sergio Afanador <sergioafanador102@gmail.com>
2024-02-05 00:36:05 -0500

    Increment quantity debugging

--- src/app/products/[id]/actions.ts
+++ src/app/products/[id]/actions.ts
@@ -3,9 +3,9 @@
 import { createCart, getCart } from "@/lib/db/cart";
 import { prisma } from "@/lib/db/prisma";
 import { revalidatePath } from "next/cache";
-import { string } from "zod";
 
-export async function incrementProductQuantity(productId: String){
+
+export async function incrementProductQuantity(productId: string){
     const cart = await getCart() ?? await createCart();
 
     const articleInCart = cart.items.find(item => item.productId === productId)
@@ -28,7 +28,7 @@ export async function incrementProductQuantity(productId: String){
             data:{
                 items:{
                     create:{
-                        productId: "",
+                        productId,
                         quantity:1,
                     },
                 },
