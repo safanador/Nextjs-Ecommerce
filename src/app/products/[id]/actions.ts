55a1c18522e856bb9a98abf3aafa7417ff3cfd0d
Sergio Afanador <sergioafanador102@gmail.com>
2024-02-27 15:06:43 -0500

    retomando proyecto

--- src/app/products/[id]/actions.ts
+++ src/app/products/[id]/actions.ts
@@ -4,7 +4,6 @@ import { createCart, getCart } from "@/lib/db/cart";
 import { prisma } from "@/lib/db/prisma";
 import { revalidatePath } from "next/cache";
 
-
 export async function incrementProductQuantity(productId: string){
     const cart = await getCart() ?? await createCart();
 
